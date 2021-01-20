978a8f396ade0d7f14edb4548d186b03aee2802b
IGaussI <eaonlpd@gmail.com>
2021-01-20 16:10:18 +0200

    a back-up version

--- back-end/mapApp/mapApp/prod_settings.py
+++ back-end/mapApp/mapApp/prod_settings.py
@@ -3,19 +3,18 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
+DEBUG = False
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'projectmap',
+        'NAME': 'She3348Jdfurfghs',
         'USER': 'www',
         'PASSWORD': 'p@ssw0rd',
         'HOST': 'localhost',
         'PORT': '',
     }
 }
-
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
--- back-end/mapApp/mapApp/urls.py
+++ back-end/mapApp/mapApp/urls.py
@@ -27,14 +27,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api_app.urls')),
+    path('apiurls/', include('api_app.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken'))
     # path('api/token', TokenObtainPairView.as_view()),
     # path('api/token/refresh', TokenRefreshView.as_view())
 
     # path('api/auth/', include('djoser.urls.authtoken')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-urlpatterns += [re_path(r'^.*', include('start.urls'))]
+]
